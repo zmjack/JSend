cc44c0c58930a5211f7094d57b459f12d73ed5a8
zmjack <jack@zmland.com>
2020-02-08 21:00:04 +0800

    v0.0.1

---
+++ JSend/JSendConst.cs
@@ -0,0 +1,10 @@
+﻿
+namespace Frontend
+{
+    public static class JSendConst
+    {
+        public const string SUCCESS_STATUS = "success";
+        public const string FAIL_STATUS = "fail";
+        public const string ERROR_STATUS = "error";
+    }
+}
---
+++ push.cmd
@@ -0,0 +1,2 @@
+nuget push "JSend/bin/Release/JSend.0.0.1.nupkg" -source nuget.org
+pause
